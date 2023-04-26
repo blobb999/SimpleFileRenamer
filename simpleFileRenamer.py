940d5b1f0aba54fc9c3f28364196b6ed96868fa6
blobb <89486466+blobb999@users.noreply.github.com>
2023-04-26 22:20:07 +0200

    Update simpleFileRenamer.py

--- simpleFileRenamer.py
+++ simpleFileRenamer.py
@@ -1,33 +1,69 @@
 import os
 import re
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from xml.dom import minidom
 from urllib.parse import quote
+from natsort import natsorted
 
-def rename_files_in_directory(directory, episode_pattern, title_pattern):
+def rename_files_in_directory(directory, episode_pattern, title_pattern, use_names_txt):
     count = 0
-    for filename in os.listdir(directory):
-        if filename.endswith('.mp4'):
-            new_name = reformat_name(filename, episode_pattern, title_pattern)
-            os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
-            count += 1
-            status_var.set(f"Renamed: {count} files")
-            root.update()
+
+    with open(os.path.join(directory, 'rename_log.txt'), 'w', encoding='utf-8') as log_file:
+        if use_names_txt:
+            with open(os.path.join(directory, 'names.txt'), 'r', encoding='utf-8') as f:
+                names = [re.split(r'\s+', line.strip(), maxsplit=1) for line in f.readlines()]
+
+        for i, filename in enumerate(natsorted(os.listdir(directory))):
+            if filename.endswith('.mp4'):
+                if use_names_txt and i < len(names):
+                    file_extension = os.path.splitext(filename)[-1]
+                    new_name = names[i][0] + " - " + re.sub(r'\s+\(.*\)', '', names[i][1]) + file_extension
+                else:
+                    episode_search = re.search(episode_pattern, filename)
+                    title_search = re.search(title_pattern, filename)
+                    if episode_search and title_search:
+                        episode_number = episode_search.group(1)
+                        title = title_search.group(1)
+                        new_name = f"{episode_number} - {title}{file_extension}"
+                    else:
+                        new_name = f"{i+1:02d} - {os.path.splitext(filename)[0]}{file_extension}"
+                os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
+                count += 1
+                status_var.set(f"Renamed: {count} files")
+                log_file.write(f"{filename} -> {new_name}\n")
+                root.update()
     return count
 
-def reformat_name(filename, episode_pattern, title_pattern):
-    episode_number = re.search(episode_pattern, filename)
-    title = re.search(title_pattern, filename)
-    
-    if episode_number and title:
-        episode_number = episode_number.group(1)
-        title = title.group(1).replace('.', ' ')
-        new_name = f"{episode_number} - {title}.mp4"
-        return new_name
+
+def undo_renaming():
+    directory = folder_path.get()
+    log_path = os.path.join(directory, 'rename_log.txt')
+
+    if not os.path.exists(log_path):
+        messagebox.showerror("Error", "rename_log.txt not found")
+        return
+
+    with open(log_path, 'r', encoding='utf-8') as log_file:
+        lines = log_file.readlines()
+
+    count = 0
+    for line in lines:
+        old_name, new_name = line.strip().split(' -> ')
+        os.rename(os.path.join(directory, new_name), os.path.join(directory, old_name))
+        count += 1
+
+    messagebox.showinfo("Result", f"Undid renaming of {count} files")
+
+def toggle_input_fields():
+    use_names_txt = use_names_txt_var.get()
+    if use_names_txt:
+        episode_entry.configure(state='disabled')
+        title_entry.configure(state='disabled')
     else:
-        return filename
+        episode_entry.configure(state='normal')
+        title_entry.configure(state='normal')
 
 def browse_directory():
     folder_selected = filedialog.askdirectory()
@@ -37,9 +73,11 @@ def start_renaming():
     directory = folder_path.get()
     episode_pattern = episode_entry.get()
     title_pattern = title_entry.get()
+    use_names_txt = use_names_txt_var.get()
 
-    count = rename_files_in_directory(directory, episode_pattern, title_pattern)
+    count = rename_files_in_directory(directory, episode_pattern, title_pattern, use_names_txt)
     messagebox.showinfo("Result", f"Renamed {count} files")
+
 
 def show_help():
     help_text = ("Episode pattern: Regular expression pattern to extract the episode number.\n"
@@ -125,6 +163,16 @@ status_var = tk.StringVar()
 status_bar = tk.Label(root, textvariable=status_var, bd=1, relief=tk.SUNKEN, anchor=tk.W)
 status_var.set("Ready")
 status_bar.grid(row=5, column=0, columnspan=3, sticky=tk.W + tk.E)
+
+# Checkbox for using names.txt
+use_names_txt_var = tk.BooleanVar()
+names_txt_checkbox = ttk.Checkbutton(root, text="Use names.txt", variable=use_names_txt_var, command=toggle_input_fields)
+names_txt_checkbox.grid(row=6, column=1)
+
+# Add the "Undo" button after the "Create Playlist" button
+undo_button = tk.Button(root, text="Undo", command=undo_renaming)
+undo_button.grid(row=4, column=2)
+
 
 # Run the GUI
 root.mainloop()
